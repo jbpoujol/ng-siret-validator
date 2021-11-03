1c58a4d567c97409661151ff51102453664acb74
Jean-Baptiste Poujol <47899256+jbpoujol@users.noreply.github.com>
2021-11-03 15:49:06 +0100

    ajout du cas unique du siège de la poste

--- projects/ng-siret-validator/src/lib/siret.validator.ts
+++ projects/ng-siret-validator/src/lib/siret.validator.ts
@@ -10,19 +10,22 @@ export function ValidateSiret(
 
   // SIRET de la poste
   if (siret.startsWith(356)) {
-    let total: number = 0;
+    // Cas unique du siège de la poste
+    if (siret == 35600000000048) return null;
+    // Tous les autres SIRET de la poste
+    let total = 0;
     for (let i = 0; i < 14; i++) {
-      let step: number = parseInt(siret[i]);
+      let step = parseInt(siret[i]);
       total += step;
     }
     return total % 5 === 0 ? null : { siret: true };
   }
 
   // SIRET classique
-  let bal: number = 0;
-  let total: number = 0;
+  let bal = 0;
+  let total = 0;
   for (let j = 14 - 1; j >= 0; j--) {
-    let step: number = (siret.charCodeAt(j) - 48) * (bal + 1);
+    let step = (siret.charCodeAt(j) - 48) * (bal + 1);
     total += step > 9 ? step - 9 : step;
     bal = 1 - bal;
   }
